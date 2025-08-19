fdc33617a505a6c0754648208605347725985237
KitziaGarcia <kitzia.daniela3101@gmail.com>
2025-08-18 22:53:59 -0700

    Rename files and improve logic
    Rename HelloApplication.java to Agenda.App
    
    Rename Scene2.fxml to Scene.fxml

--- AgendaApp.java
+++ AgendaApp.java
@@ -1,16 +1,14 @@
 package com.example.agenda;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class AgendaApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene2.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AgendaApp.class.getResource("Scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.sizeToScene();
